9c2c7f5d0f92f405056a62825f6c1a12b2c684ed
wuhong <wuh131420@foxmail.com>
2018-07-05 12:28:52 +0800

    2018-07-05-提交测试文档-个人中心测试问题

--- pages/account/contact/contact.js
+++ pages/account/contact/contact.js
@@ -77,58 +77,12 @@ Page({
       let error = res.error == 0 ? 'success' : 'error'
       app.toastMsg(error,res.msg)
       if(res.error == 0){
-        wx.navigateBack({
-          delta:1
-        })
+        setTimeout(()=>{
+          wx.navigateBack({
+            delta:1
+          })
+        },1000)
       }
     })
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
